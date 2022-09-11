e528f6b87b41dc87828f6e16294fbece2db4c2cf
L33T-AF-Original <92133003+L33T-AF-Original@users.noreply.github.com>
2022-09-11 14:08:46 -0400

    Add files via upload
    Squared.

---
+++ help.py
@@ -0,0 +1,71 @@
+)import math
+import sys
+
+x = 2
+z = x**2
+lower=1
+b=2**12
+y=z*x
+w=y*x
+v=y*z
+t=w*z
+s=w*y
+r=w*w
+#for x in range(lower,upper):
+#    if(x%2==0 and z%2==0):
+#        print(z**2) 
+u=r*x
+q=r*z
+a=r*y
+c=b*x
+d=b*z
+m=b*y
+e=m*c**2
+f=b*w
+g=b*v
+h=b*t
+i=b*s
+j=b*r
+k=b*u
+l=b*q
+n=b*a
+o=b*c
+p=b*d
+
+print(x)
+print(z)
+print(y)
+print(w)
+print(v)
+print(t)
+print(s)
+print(r)
+print(u)
+print(q)
+print(a)
+print(b)
+print(c)
+print(d)
+print(m)
+print(f)
+print(g)
+print(h)
+print(i)
+print(j)
+print(k)
+print(l)
+print(n)
+print(o)
+print(p)
+print(e)
+print(2**26)
+
+#print(2**128)
+#print(sys.argv)
+
+#help(math)
+
+print ("Hello, World!")
+
+
+exit
