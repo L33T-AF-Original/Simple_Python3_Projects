7f41e93269d75b4f6f8f9be2f9d8222fb6c5f89b
L33T-AF-Original <92133003+L33T-AF-Original@users.noreply.github.com>
2022-09-11 13:34:05 -0400

    Add files via upload

---
+++ times_tables.py
@@ -0,0 +1,119 @@
+# imports first
+import math
+from math import pi
+# global variables, classes, defs, functions, 
+lower= 2
+upper= 20**4
+i = lower
+x,y=0,1
+_upper= 20**5
+upper_=2**128+1
+#in_ = int(  put("What multiplication table do u need help with? "))
+#help(range) 
+#ls_ = [1,2,3,4,5,6,7,8,9,10,11,12]
+#x=1
+#arg = x+1
+#for in_ in range (lower,upper):
+#    if(in_):
+#        print(in_ + " * " + int(ls_.index(arg)) + " = ")
+
+#        print(in_)
+print("Times tables.")
+print("\n2's \n")
+for i in range (lower,upper): 
+    if(i%2==0 and i<=24): 
+        #print(list(str(i)) 
+        print(i)
+print("\n3's \n")
+
+for i in range (lower,upper): 
+    if(i%3==0 and i<=36): 
+        print(i)	
+print("\n4's \n")
+
+for i in range (lower,upper):
+    if(i%4==0 and i<=48):
+        print(i)
+print("\n5's \n")
+
+for i in range (lower,upper): 
+    if(i%5==0 and i<=60):   
+        print(i)
+print("\n6’s \n")
+
+for i in range (lower,upper): 
+    if(i%6==0 and i<=72):   
+        print(i)
+print("\n7’s \n")
+
+for i in range (lower,upper): 
+    if(i%7==0 and i<=84):   
+        print(i)
+print("\n8’s \n")
+
+for i in range (lower,upper): 
+    if(i%8==0 and i<=96):   
+        print(i)
+print("\n9’s \n")
+
+for i in range (lower,upper): 
+    if(i%9==0 and i<=108):   
+        print(i)
+print("\n10’s \n")
+
+for i in range (lower,upper): 
+    if(i%10==0 and i<=120):   
+        print(i)
+print("\n11’s \n")
+
+for i in range (lower,upper): 
+    if(i%11==0 and i<=132):   
+        print(i)
+print("\n12's \n")
+
+for i in range (lower,upper): 
+    if(i%12==0 and i<=144):   
+        print(i)
+print("\n15's \n")
+
+for i in range (lower,upper):
+    if(i%15==0 and i<=20**2):
+        print(i)
+print("\n20's \n")
+for i in range (lower,upper):
+    if(i%4==0 and i%5==0 and i<=20**2):
+        print(i)
+
+print("\n25's \n")
+for i in range(lower,upper): 
+    if (i%25==0 and i%5==0 and i<=25**2): 
+#        nl.append(str(x))
+#        print ('\n') 
+        print(i)
+        
+print("\n25,000's \n")
+for i in range(lower,_upper):
+    if (i%25000==0):
+        print(i)
+    
+print("\n")
+print("It's over 11449921622079721154497199899966677907638995896622219388843656194870204440338362332943924986323117322375295462212094869620734133568 Lol. \n")
+print("2**128*99999=")
+print(2**128*99999)
+print("2**128")
+print(2**128)
+print()
+print(34027896409726925407873997368569389377388544)
+#for y in range(lower,upper_):
+#    if(y%2**2==0):
+#        print(y) 
+
+print("20**100=")
+print(20**100)
+print()
+while y<20**100: 
+    print(y) 
+    x,y = y,x+y
+
+
+exit
